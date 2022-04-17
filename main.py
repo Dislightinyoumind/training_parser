9497ef319717b814f6506fc4d97e35b3b91a8f91
DISLIGHT <mr.syrev99@mail.ru>
2022-04-17 17:37:09 +0300

    file structure

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+	keywords = ("DATES", "COMPDAT", "COMPDATL")
+	road_to_input = "input_data/input.inc"
+	road_to_output = "output_data/output.csv"
