bdb27fe451691ee551d84eaf91ff75415161b82a
DISLIGHT <mr.syrev99@mail.ru>
2022-04-19 03:20:40 +0300

    pre_parser

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
+from Lib import Executor_fun
 
 
 if __name__ == "__main__":
 	keywords = ("DATES", "COMPDAT", "COMPDATL")
+	parameters = ("Date", "Well name", "Local grid name", "I", "J", "K upper", "K lower", "Flag on connection",
+				  "Saturation table", "Transmissibility factor", "Well bore diameter", "Effective Kh",
+				  "Skin factor", "D-factor")
 	road_to_input = "input_data/input.inc"
 	road_to_output = "output_data/output.csv"
+
+
+
+
+
+
